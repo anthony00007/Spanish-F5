97c8eb9b07e8c6ff31f17235ef1deb620740c713
anthony00007 <anthonycosmicoyreal@gmail.com>
2024-11-18 08:51:55 -0400

    Update finetune_cli.py

--- src/f5_tts/train/finetune_cli.py
+++ src/f5_tts/train/finetune_cli.py
@@ -81,19 +81,19 @@ def main():
         model_cls = DiT
         model_cfg = dict(dim=1024, depth=22, heads=16, ff_mult=2, text_dim=512, conv_layers=4)
         if args.finetune:
-            if args.pretrain is None:
+            if args.file_checkpoint_train is None:
                 ckpt_path = str(cached_path("hf://SWivid/F5-TTS/F5TTS_Base/model_1200000.pt"))
             else:
-                ckpt_path = args.pretrain
+                ckpt_path = args.file_checkpoint_train
     elif args.exp_name == "E2TTS_Base":
         wandb_resume_id = None
         model_cls = UNetT
         model_cfg = dict(dim=1024, depth=24, heads=16, ff_mult=4)
         if args.finetune:
-            if args.pretrain is None:
+            if args.file_checkpoint_train is None:
                 ckpt_path = str(cached_path("hf://SWivid/E2-TTS/E2TTS_Base/model_1200000.pt"))
             else:
-                ckpt_path = args.pretrain
+                ckpt_path = args.file_checkpoint_train
 
     if args.finetune:
         if not os.path.isdir(checkpoint_path):
