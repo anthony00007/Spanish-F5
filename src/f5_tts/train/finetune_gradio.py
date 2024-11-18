d9d1f7098d0dd39fa14ab68499d2ade96ee9e24e
anthony00007 <anthonycosmicoyreal@gmail.com>
2024-11-18 10:47:15 -0400

    Update finetune_gradio.py

--- src/f5_tts/train/finetune_gradio.py
+++ src/f5_tts/train/finetune_gradio.py
@@ -1422,7 +1422,7 @@ Skip this step if you have your dataset, metadata.csv, and a folder wavs with al
             )
 
             audio_speaker = gr.File(label="Voice", type="filepath", file_count="multiple")
-            txt_lang = gr.Text(label="Language", value="English")
+            txt_lang = gr.Text(label="Language", value="Spanish")
             bt_transcribe = bt_create = gr.Button("Transcribe")
             txt_info_transcribe = gr.Text(label="Info", value="")
             bt_transcribe.click(
